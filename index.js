707ab32e6ffe339792d0b27d0bd13b515aeb679d
Ryan Wilson <ryanswilson1@gmail.com>
2020-10-11 19:31:35 -0400

    added questions user will be prompted to answer to create readme file

--- index.js
+++ index.js
@@ -2,6 +2,49 @@ const inquirer = require("inquirer");
 const axios = require("axios");
 const fs = require('fs');
 const path = require('path');
+inquirer
+    .prompt([
+        {
+            type: "input",
+            message: "What is your GitHub user name?",
+            name: "userName"
+        },
+        {
+            type: "input",
+            message: "What is the title of your project?",
+            name: "projectTitle"
+        },
+        {
+            type: "input",
+            message: "List a detailed description of your project or web application",
+            name: "applicationDetails"
+        },
+        {
+            type: "input",
+            message: "List a Table of Contents for your application",
+            name: "tableOfContents"
+        },
+        {
+            type: "input",
+            message: "Provide any details necessary for installation of the application",
+            name: "installation"
+        },
+        {
+            type: "input",
+            message: "Provide Usage Information",
+            name: "usageInformation"
+        },
+        {
+            type: "input",
+            message: "List guidelines for contribution",
+            name: "contributionGuidelines"
+        },
+        {
+            type: "input",
+            message: "List instructions for testing the application",
+            name: "test"
+        }
+    ]);
 // array of questions for user
 const questions = [
 
